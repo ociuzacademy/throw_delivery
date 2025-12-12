083a847d4b6f5de2c5fe5a42484d2ecadefa629e
Mikhil Mohan C <mikhilmohan13@gmail.com>
2025-12-12 13:55:42 +0530

    feat: Add Firebase Core integration and configuration files.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:throw_delivery/modules/splash_screen_module/view/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
